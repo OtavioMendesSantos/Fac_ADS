112d5fa4eafefeae6b06fd61ed566ca3c267c1b4
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-09-04 23:22:39 -0300

    :truck: chore

---
+++ 2-Semestre/LinguagemTecnicaDeProgramacao-Python/Atividades/atv1/atv1-5.py
@@ -0,0 +1,8 @@
+# Encontrar o dobro de um número caso ele seja positivo e o seu triplo caso seja negativo, imprimindo o resultado.
+
+num = int(input("Digite um número: "))
+
+if num > 0:
+    print(num*2)
+else:
+    print(num*3)
